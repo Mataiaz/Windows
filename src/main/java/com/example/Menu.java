04654fc0de82e90fcb513b548a56e303eb0d083c
Mataiaz <mathias344@live.no>
2022-06-15 22:37:32 +0200

    added a simple window

--- src/main/java/com/example/Menu.java
+++ src/main/java/com/example/Menu.java
@@ -1,32 +1,35 @@
 package com.example;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import java.net.*;
+import java.awt.Color;
 
-import javax.swing.JCheckBox;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JProgressBar;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 public class Menu extends JFrame{
     private JPanel contentPane;
-    private JLabel lblA;
     public static void main(String[] args) {
             Menu frame = new Menu();
             frame.setVisible(true);
     }
 
+    //This will be the starting window where we pick options and paths to other windows
     public Menu () 
     {
+        //Enables us to exit the program when pressing close on just the window
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Here we set the starting size, color and name
         setBounds(100, 100, 450, 300);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
-        contentPane.add();
-        
+        contentPane.setBackground(Color.darkGray);
+        TitledBorder tBorder = BorderFactory.createTitledBorder("This is a title border!");
+        tBorder.setTitleColor(Color.BLACK);
+        contentPane.setBorder(tBorder);
     }
 }
